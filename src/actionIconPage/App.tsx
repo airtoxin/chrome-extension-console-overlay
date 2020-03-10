d186b5201c539f87efbc483d2356ee3a5192bd62
airtoxin <airtoxin@icloud.com>
2020-03-10 17:08:36 +0900

    Remove default view

--- src/actionIconPage/App.tsx
+++ src/actionIconPage/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import update from "immutability-helper";
 
@@ -116,20 +115,6 @@ function App() {
           )}
         </tbody>
       </table>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
