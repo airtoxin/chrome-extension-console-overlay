8c6987f52f4a6a55731d222294adf2b7b66e808c
airtoxin <airtoxin@icloud.com>
2020-03-11 16:13:03 +0900

    Fix proxy

--- src/contentScript/proxyConsole.ts
+++ src/contentScript/proxyConsole.ts
@@ -3,33 +3,39 @@ import EventEmitter from "events";
 export const proxyConsole = (): EventEmitter => {
   const ee = new EventEmitter();
   console.trace = new Proxy(console.trace, {
-    apply(_target, _thisArg, argArray) {
+    apply(target, _thisArg, argArray) {
       ee.emit("console", "trace", argArray);
+      target(...argArray);
     }
   });
   console.debug = new Proxy(console.debug, {
-    apply(_target, _thisArg, argArray) {
+    apply(target, _thisArg, argArray) {
       ee.emit("console", "debug", argArray);
+      target(...argArray);
     }
   });
   console.info = new Proxy(console.info, {
-    apply(_target, _thisArg, argArray) {
+    apply(target, _thisArg, argArray) {
       ee.emit("console", "info", argArray);
+      target(...argArray);
     }
   });
   console.log = new Proxy(console.log, {
-    apply(_target, _thisArg, argArray) {
+    apply(target, _thisArg, argArray) {
       ee.emit("console", "log", argArray);
+      target(...argArray);
     }
   });
   console.warn = new Proxy(console.warn, {
-    apply(_target, _thisArg, argArray) {
+    apply(target, _thisArg, argArray) {
       ee.emit("console", "warn", argArray);
+      target(...argArray);
     }
   });
   console.error = new Proxy(console.error, {
-    apply(_target, _thisArg, argArray) {
+    apply(target, _thisArg, argArray) {
       ee.emit("console", "error", argArray);
+      target(...argArray);
     }
   });
 
